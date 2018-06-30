6422601140ff6215254d5bd01278ef1022dc9167
SuperJura <jura1994jk@hotmail.com>
2018-06-30 12:11:03 +0200

    new build!

--- components/button/index.js
+++ components/button/index.js
@@ -7,7 +7,7 @@ export default class Button extends React.Component
 	render() 
 	{
 		const isSmall = this.props.isSmall !== undefined;
-		const classes = [styles.button, isSmall ? styles.small : undefined];
+		const classes = [styles.button, this.props.classes, isSmall ? styles.small : undefined];
 
 		const button = (
 			<input onClick={this.props.onClick} className={classes.join(' ')} disabled={this.props.disabled} type='button' value={this.props.children} />
